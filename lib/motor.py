cc7f9d118d4c75952d9146e65558be62426dba46
Cesar Rodriguez <kesarr@gmail.com>
2020-04-24 13:51:05 +0200

    🚀

--- lib/motor.py
+++ lib/motor.py
@@ -26,35 +26,32 @@ def print_board_status():
 
 if __name__ == "__main__":
 
-  board_detect()    # If you forget address you had set, use this to detected them, must have class instance
+  board_detect()
 
-  # Set board controler address, use it carefully, reboot module to make it effective
-  '''
-  board.set_addr(0x10)
-  if board.last_operate_status != board.STA_OK:
-    print("set board address faild")
-  else:
-    print("set board address success")
-  '''
-
-  while board.begin() != board.STA_OK:    # Board begin and check board status
+  while board.begin() != board.STA_OK:
     print_board_status()
     print("board begin faild")
     time.sleep(2)
   print("board begin success")
 
-  board.set_encoder_enable(board.ALL)                 # Set selected DC motor encoder enable
+  # board.set_encoder_enable(board.ALL)                 # Set selected DC motor encoder enable
   # board.set_encoder_disable(board.ALL)              # Set selected DC motor encoder disable
-  board.set_encoder_reduction_ratio(board.ALL, 43)    # Set selected DC motor encoder reduction ratio, test motor reduction ratio is 43.8
+  # board.set_encoder_reduction_ratio(board.ALL, 43)    # Set selected DC motor encoder reduction ratio, test motor reduction ratio is 43.8
 
   board.set_moter_pwm_frequency(1000)   # Set DC motor pwm frequency to 1000HZ
 
+  board.motor_movement([board.M1], board.CW, 5)
+  board.motor_movement([board.M2], board.CW, 5)
+  time.sleep(1)
+  board.motor_stop(board.ALL)
+
+'''
   while True:
     for duty in range(5, 95, 10):   # slow to fast
       board.motor_movement([board.M1], board.CW, duty)    # DC motor 1 movement, orientation clockwise
       board.motor_movement([board.M2], board.CCW, duty)   # DC motor 2 movement, orientation count-clockwise
       time.sleep(1)
-      speed = board.get_encoder_speed(board.ALL)      # Use boadrd.all to get all encoders speed
+      speed = board.get_encoder_speed(board.ALL)
       print("duty: %d, M1 encoder speed: %d rpm, M2 encoder speed %d rpm" %(duty, speed[0], speed[1]))
 
     for duty in range(95, 5, - 10):   # fast to low
@@ -68,3 +65,4 @@ if __name__ == "__main__":
     board.motor_stop(board.ALL)   # stop all DC motor
     print_board_status()
     time.sleep(4)
+'''
